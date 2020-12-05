9dc8dc91a27678d486023b78bfbc0bb1211d20eb
Jady Hom <hyjady@gmail.com>
2020-12-05 14:22:39 -0500

    fixed createlike, getlikes, and routes

--- routes/LikeRouter.js
+++ routes/LikeRouter.js
@@ -1,7 +1,7 @@
 const Router = require("express").Router();
 const controller = require("../controllers/LikeController");
 
-Router.post("/create/:post_id", controller.CreateLike);
+Router.post("/create/:post_id/:user_id", controller.CreateLike);
 Router.get("/getlikes/:post_id", controller.GetLikes);
 Router.get("/:like_id", controller.DeleteLikes);
 
