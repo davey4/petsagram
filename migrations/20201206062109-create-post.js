ffd250761cec5aa67f32df32021e34d01e25645e
Tyler Klafke <Ttylerrk@gmail.com>
2020-12-06 22:56:53 -0800

    finished associations and got migration framework setup

---
+++ migrations/20201206062109-create-post.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      likes_id: {
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("posts");
+  },
+};
---
+++ models/comments.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Comments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Comments.belongsTo(models.User, {
+        foreignKey: "user_id",
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      });
+      Comments.belongsTo(models.Post, {
+        foreignKey: "post_id",
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      });
+    }
+  }
+  Comments.init(
+    {
+      description: DataTypes.STRING,
+      post_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Comments",
+      tableName: "comments",
+    }
+  );
+  return Comments;
+};
