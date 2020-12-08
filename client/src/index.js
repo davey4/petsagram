4a9b685325b9c83157a0987cd5f1826b068ffde2
Jady Hom <hyjady@gmail.com>
2020-12-08 15:34:44 -0500

    import react-md

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
+import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Configuration } from "@react-md/layout";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Configuration>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Configuration>
   </React.StrictMode>,
   document.getElementById("root")
 );
